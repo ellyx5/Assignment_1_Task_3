a9e2f9af927193b9f79d7f0fc7b0e69b9fa924a8
ellyx5 <80619860+ellyx5@users.noreply.github.com>
2021-04-19 20:45:49 +1000

    Update Q2d.sql
    To make it look more presentable

--- Q2d.sql
+++ Q2d.sql
@@ -1,1 +1,6 @@
-SELECT distinct m.LastName, m.FirstName, m.MemberID FROM Member m, Entry e WHERE m.MemberID = e.MemberID AND EXISTS (SELECT * from Entry y WHERE m.MemberID = y.MemberID AND Year=2013) AND EXISTS (SELECT * from Entry y WHERE m.MemberID = y.MemberID AND Year=2014) AND EXISTS (SELECT * from Entry y WHERE m.MemberID = y.MemberID AND Year=2015);
+SELECT distinct m.LastName, m.FirstName, m.MemberID 
+FROM Member m, Entry e 
+WHERE m.MemberID = e.MemberID 
+AND EXISTS (SELECT * from Entry y WHERE m.MemberID = y.MemberID AND Year=2013)
+AND EXISTS (SELECT * from Entry y WHERE m.MemberID = y.MemberID AND Year=2014) 
+AND EXISTS (SELECT * from Entry y WHERE m.MemberID = y.MemberID AND Year=2015);
