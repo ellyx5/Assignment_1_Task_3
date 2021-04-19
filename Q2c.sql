6ba0e7a51e7ca12cbd825bb651e02aa9ec7f3920
ellyx5 <80619860+ellyx5@users.noreply.github.com>
2021-04-19 20:47:20 +1000

    Updata - Formatting
    To make it more presentable

--- Q2c.sql
+++ Q2c.sql
@@ -1,1 +1,4 @@
-SELECT distinct m.LastName, m.FirstName, m.MemberID FROM Member m, Entry e WHERE m.MemberID = e.MemberID AND NOT EXISTS (SELECT * from Entry y WHERE m.MemberID = y.MemberID AND Year=2013);
+SELECT distinct m.LastName, m.FirstName, m.MemberID
+FROM Member m, Entry e 
+WHERE m.MemberID = e.MemberID 
+AND NOT EXISTS (SELECT * from Entry y WHERE m.MemberID = y.MemberID AND Year=2013);
